913cd770fb44ce21c23232e39004579fbd5c7825
hyy <376246826@qq.com>
2015-08-14 16:36:49 +0800

    Fix

--- app/controllers/WeixinContoller.php
+++ app/controllers/WeixinContoller.php
@@ -19,6 +19,6 @@ class WeixinContoller extends BaseController{
     
     public function response_token(){
 
-        return Response::make( self::checkInput::get( 'echostr ') : 'Error' );
+        return Response::make( self::check_signature() ? Input::get( 'echostr ') : 'Error' );
     }
 }
