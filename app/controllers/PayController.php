eecc5feb04c2422fab9996144d41a3eb00600842
hyy <376246826@qq.com>
2015-08-16 18:40:07 +0800

    Add exception handler

--- app/controllers/PayController.php
+++ app/controllers/PayController.php
@@ -7,38 +7,37 @@ class PayController extends BaseController{
     
     public function generate_indent(){
 
-        $tools = new JsApiPay();
-
-        $openId = $tools->GetOpenid();
-
-        // 商户号 + 用户id + uniqid生成的随机字符串
-        $out_trade_no = WxPayConfig::MCHID.uniqid( Session::get( 'user.id' ) );
-
-        // 统一下单
-        $input = new WxPayUnifiedOrder();
-        $input->SetBody( "test" );
-        $input->SetAttach( "test" );
-        $input->SetOut_trade_no( $out_trade_no );
-        $input->SetTotal_fee( "0.01" );
-        $input->SetTime_start( date( "YmdHis" ) );
-        $input->SetTime_expire( date( "YmdHis", time() + 3600 ) );
-        $input->SetGoods_tag( "test" );
-        $input->SetNotify_url( "http://120.25.211.179/pay/notify" );
-        $input->SetTrade_type( "JSAPI" );
-        $input->SetOpenid( $openId );
-        $order = WxPayApi::unifiedOrder($input);
-
         // 
         try{
+            $tools = new JsApiPay();
+
+            $openId = $tools->GetOpenid();
+
+            // 商户号 + 用户id + uniqid生成的随机字符串
+            $out_trade_no = WxPayConfig::MCHID.uniqid( Session::get( 'user.id' ) );
+
+            // 统一下单
+            $input = new WxPayUnifiedOrder();
+            $input->SetBody( "test" );
+            $input->SetAttach( "test" );
+            $input->SetOut_trade_no( $out_trade_no );
+            $input->SetTotal_fee( "0.01" );
+            $input->SetTime_start( date( "YmdHis" ) );
+            $input->SetTime_expire( date( "YmdHis", time() + 3600 ) );
+            $input->SetGoods_tag( "test" );
+            $input->SetNotify_url( "http://120.25.211.179/pay/notify" );
+            $input->SetTrade_type( "JSAPI" );
+            $input->SetOpenid( $openId );
+            $order = WxPayApi::unifiedOrder($input);
+
             $jsApiParameters = $tools->GetJsApiParameters($order);
         }catch( WxPayException $e ){
             
-            return Response::json(array( 'error_code' => 2, 'message' => '支付请求异常' ));
+            return Response::json(array( 'error_code' => 2, 'message' => $e->getMessage() ));
         }catch( Exception $e ){
 
-            return Response::json(array( 'error_code' => 1, 'message' => '未知错误' ));
+            return Response::json(array( 'error_code' => 1, 'message' => $e->getMessage() ));
         }
-        
 
         return Response::json(array( 'error_code' => 0, 'parameters' => $jsApiParameters ));
     }
