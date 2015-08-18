1928d446e210aa28e0325ede2f27e7618abbed2e
hyy <376246826@qq.com>
2015-08-18 15:22:39 +0800

    Fix

--- app/controllers/PayController.php
+++ app/controllers/PayController.php
@@ -14,35 +14,21 @@ class PayController extends BaseController{
             return Redirect::to( $url );
         }
 
-		$open_id = Session::get( 'user.open_id' );
+		$open_id = Session::pull( 'user.open_id' );
 		if ( !isset( $open_id ) ){
 			$open_id = $tools->GetOpenidFromMp( Input::get( 'code') );
 			Session::put( 'user.open_id', $open_id );
 		}
 
 		//Log::info( 'Code before: '.Input::get('code') );
-
-        // 商户号 + 用户id + uniqid生成的随机字符串
-        $out_trade_no = WxPayConfig::MCHID.uniqid( Session::get( 'user.id' ) );
-
-        // 需重置下当前时区，PHP配置文件不知为何不起作用
-        date_default_timezone_set('PRC');
-
-        // 统一下单
-        $input = new WxPayUnifiedOrder();
-        $input->SetBody( "test" );
-        $input->SetAttach( "test" );
-        $input->SetOut_trade_no( $out_trade_no );
-        $input->SetTotal_fee( 100 );
-        $input->SetTime_start( date( 'YmdHis' ) );
-        $input->SetTime_expire( date( 'YmdHis', time() + 3600 ) );
-        $input->SetGoods_tag( "test" );
-        $input->SetNotify_url( "http://test.zerioi.com/pay/notify" );
-        $input->SetTrade_type( "JSAPI" );
-        $input->SetOpenid( $open_id );
-        $order = WxPayApi::unifiedOrder($input);
-        $js_api_parameters = $tools->GetJsApiParameters($order);
-
+        $js_api_parameters = Session::pull( 'user.js_api_parameters' )
+        if ( !isset( $js_api_parameters ) ){
+            $js_api_parameters = $this->create_order( $open_id );
+            Session::put( 'user.js_api_parameters', json_encode( $js_api_parameters ) );
+        }else{
+            $js_api_parameters = json_decode( $js_api_parameters );
+        }
+        
         $period_id = Input::get( 'period_id' );
         $period = Period::find( $period_id );
 
@@ -66,8 +52,41 @@ class PayController extends BaseController{
         return View::make( 'register.pay', $data );
     }
 
+    protected function create_order( $open_id ){
+
+        // 商户号 + 用户id + uniqid生成的随机字符串
+        $out_trade_no = WxPayConfig::MCHID.uniqid( Session::get( 'user.id' ) );
+
+        // 需重置下当前时区，PHP配置文件不知为何不起作用
+        date_default_timezone_set('PRC');
+
+        // 统一下单
+        $input = new WxPayUnifiedOrder();
+        $input->SetBody( "test" );
+        $input->SetAttach( "test" );
+        $input->SetOut_trade_no( $out_trade_no );
+        $input->SetTotal_fee( 1 );
+        $input->SetTime_start( date( 'YmdHis' ) );
+        $input->SetTime_expire( date( 'YmdHis', time() + 3600 ) );
+        $input->SetGoods_tag( "test" );
+        $input->SetNotify_url( "http://test.zerioi.com/pay/notify" );
+        $input->SetTrade_type( "JSAPI" );
+        $input->SetOpenid( $open_id );
+        $order = WxPayApi::unifiedOrder($input);
+        $js_api_parameters = $tools->GetJsApiParameters($order);
+
+        return $js_api_parameters;
+    }
+
     public function notify(){
 
+        $request = Request::instance();
+
+        $message = $request->getContent();
+
+        Log::info( $message );
+
+        return '<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>';
     }
 
 }
