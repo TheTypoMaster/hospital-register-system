f436524c577aa0dafaa3386030f721725ac739a1
hyy <376246826@qq.com>
2015-08-22 17:19:08 +0800

    完成主模板

--- app/views/layouts/master_web.blade.php
+++ app/views/layouts/master_web.blade.php
@@ -22,17 +22,34 @@
                 <div class="top-right">
                     <img class="photo" src="/images/doc_web/u8.png" alt="王磊">
                     <span class="name">王磊</span>
-                    
                 </div>
             </div>
-            <div class="main">
+            <div class="main clearfix">
                 <div class="navi">
-                    <div class="sub-btn odd"></div>
-                    <div class="sub-btn even"></div>
-                    <div class="sub-btn odd"></div>
-                    <div class="sub-btn even"></div>
-                    <div class="sub-btn odd"></div>
-                    <div class="sub-btn even"></div>
+                    <div class="sub-btn odd">
+                        <img class="sub-icon" src="/images/icons/account.png">
+                        <a class="sub-tl" href="/doc/home/account">个人账户</a>
+                    </div>
+                    <div class="sub-btn even">
+                        <img class="sub-icon" src="/images/icons/patient.png">
+                        <a class="sub-tl" href="/doc/home/patient">挂号病人</a>
+                    </div>
+                    <div class="sub-btn odd">
+                        <img class="sub-icon" src="/images/icons/chat.png">
+                        <a class="sub-tl" href="/doc/home/chat">复诊资讯</a>
+                    </div>
+                    <div class="sub-btn even">
+                        <img class="sub-icon" src="/images/icons/comment.png">
+                        <a class="sub-tl" href="/doc/home/comment">病人评价</a>
+                    </div>
+                    <div class="sub-btn odd">
+                        <img class="sub-icon" src="/images/icons/advice.png">
+                        <a class="sub-tl" href="/doc/home/advice">病人医嘱</a>
+                    </div>
+                    <div class="sub-btn even">
+                        <img class="sub-icon" src="/images/icons/message.png">
+                        <a class="sub-tl" href="/doc/home/message">消息提醒</a>
+                    </div>
                 </div>
                 <div class="main-content">
                     @section('main-content')
