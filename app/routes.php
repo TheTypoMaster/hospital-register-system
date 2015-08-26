ef646b56f8688ff6d82656171d70a7382caff01c
jv-juven <juven-jv@foxmail.com>
2015-08-26 16:56:25 +0800

    还原路由

--- app/routes.php
+++ app/routes.php
@@ -10,9 +10,6 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-Route::get("/", function(){
-    return Response::view("doctor.advice");
-});
 
 // 给公众号绑定域名所用接口
 //Route::get( '/', 'WeixinController@response_token' );
